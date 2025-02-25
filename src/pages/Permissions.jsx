d6c540fb2650b5e3bea75fdb85362dd01978badf
Angelo Galope <105281585+angelogalope@users.noreply.github.com>
2025-02-25 15:43:17 +0800

    Checkpoint

--- src/pages/Permissions.jsx
+++ src/pages/Permissions.jsx
@@ -51,56 +51,56 @@ export default function Permissions() {
 	};
 
 	return (
-		<div className="flex flex-col items-center bg-primGreen h-screen justify-center gap-2 p-6 relative pb-28">
-			<div className="flex flex-col justify-start items-start gap-6">
+		<div className="flex flex-col items-center bg-primGreen h-screen justify-center gap-2 p-8 md:p-10 relative pb-28">
+			<div className="flex flex-col justify-start items-start gap-4 md:gap-6">
 				<button onClick={handleBack}>
 					<IoArrowBack
 						size={32}
 						className="text-gray-500 bg-white p-2 rounded-full"
 					/>
 				</button>
-				<div className="flex flex-col gap-4">
-					<h1 className="text-3xl font-bold text-white">Permissions</h1>
-					<p className="text-white">
+				<div className="flex flex-col gap-2 md:gap-4">
+					<h1 className="text-2xl md:text-3xl font-bold text-white">Permissions</h1>
+					<p className="text-sm md:text-base text-white">
 						To enhance your experience, our app requires access to your camera
 						for AR navigation, your location for accurate campus navigation, and
 						notifications for real-time updates. Please grant the necessary
 						permissions to continue.
 					</p>
 				</div>
-				<div className="flex flex-col gap-4">
-					<button onClick={handleNotificationPermission} className="flex items-center gap-5 bg-white bg-opacity-25 p-4 rounded-xl">
+				<div className="flex flex-col gap-2 md:gap-4">
+					<button onClick={handleNotificationPermission} className="flex items-center gap-3 bg-white bg-opacity-25 p-3 rounded-xl">
 						<div className="text-white">
 							<IoNotificationsOutline size={30} />
 						</div>
 						<div className="text-white flex flex-col items-start">
 							<h1 className="font-semibold text-lg">Notification</h1>
-							<p className="text-start">Enables iTour to send timely updates and real-time alerts</p>
+							<p className="text-start text-sm md:text-base">Enables iTour to send timely updates and real-time alerts</p>
 						</div>
 					</button>
-					<button onClick={handleCameraPermission} className="flex items-center gap-5 bg-white bg-opacity-25 p-4 rounded-xl">
+					<button onClick={handleCameraPermission} className="flex items-center gap-3 bg-white bg-opacity-25 p-3 rounded-xl">
 						<div className="text-white">
 							<IoCameraOutline size={30} />
 						</div>
 						<div className="text-white flex flex-col items-start">
 							<h1 className="font-semibold text-lg">Camera</h1>
-							<p className="text-start">Enables iTour to access your camera for AR navigation</p>
+							<p className="text-start text-sm md:text-base">Enables iTour to access your camera for AR navigation</p>
 						</div>
 					</button>
-					<button onClick={handleLocationPermission} className="flex items-center gap-5 bg-white bg-opacity-25 p-4 rounded-xl">
+					<button onClick={handleLocationPermission} className="flex items-center gap-3 bg-white bg-opacity-25 p-3 rounded-xl">
 						<div className="text-white">
 							<IoLocationOutline size={30} />
 						</div>
 						<div className="text-white flex flex-col items-start">
 							<h1 className="font-semibold text-lg">Location</h1>
-							<p className="text-start">Enables iTour to provide accurate navigation based on your location</p>
+							<p className="text-start text-sm md:text-base">Enables iTour to provide accurate navigation based on your location</p>
 						</div>
 					</button>
 				</div>
 			</div>
-			<div className="w-full absolute bottom-10 px-6">
+			<div className="w-full absolute bottom-20 px-8 md:px-10">
 				<button
-					className="bg-primYellow w-full max-w-md py-3 rounded-full font-bold text-xl text-black"
+					className="bg-primYellow w-full max-w-md py-3 rounded-full font-bold text-lg md:text-xl text-black"
 					onClick={handleContinue}
 				>
 					Continue
