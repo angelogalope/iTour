1ab2a705eb065284d66019840ab5471ace204be8
Angelo Galope <angelo.galope321@gmail.com>
2025-04-09 23:57:08 +0800

    Minor adjustments in the Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -34,41 +34,41 @@ export default function Dashboard() {
     <div className="bg-primWhite min-h-screen w-full flex flex-col pb-28">
       <div className="h-[95vh] overflow-y-auto overflow-x-hidden no-scrollbar flex flex-col w-full">
 
-        <div className="flex flex-col w-full items-center justify-between mt-10 px-6">
-          <div className="flex items-center w-full justify-between mb-8">
-            <h1 className="text-2xl font-semibold">Hello there!</h1>
-            <button onClick={handleSideBar} className="fixed right-6 flex items-center rounded-2xl p-4 z-40 bg-white shadow-gray-500 shadow-md">
-              <CgMenuRight size={24} />
+        <div className="flex flex-col w-full items-center justify-between mt-10 mb-4 px-6">
+          <div className="flex items-center w-full justify-between">
+            <h1 className="text-xl font-semibold">Hello there!</h1>
+            <button onClick={handleSideBar} className="fixed right-6 flex items-center rounded-lg p-2 z-40 bg-white shadow-gray-500 shadow-md">
+              <CgMenuRight size={20} />
             </button>
             <SideBar isOpen={isSideBarOpen} onClose={handleSideBar} activePage={activePage} handleNavigation={handleNavigation} />
           </div>
         </div>
 
-        <div className="pb-16 flex flex-col gap-8">
-          <div className="flex flex-col gap-8 px-6">
-            <h1 className="text-5xl font-black">Find Your Destination</h1>
+        <div className="pb-16 flex flex-col gap-4">
+          <div className="flex flex-col gap-4 px-6">
+            <h1 className="text-4xl font-black">Find Your Destination</h1>
             <div className="flex flex-col gap-4">
-              <div className="flex p-4 items-center text-gray-500 font-semibold bg-white rounded-lg shadow-gray-500 shadow-md">
+              <div className="flex p-3 items-center text-gray-500 font-semibold bg-white rounded-lg shadow-gray-500 shadow-md">
                 <input type="text" placeholder="Search" className="w-full text-lg outline-none bg-transparent text-black font-normal" />
                 <PiMagnifyingGlassBold size={22} />
               </div>
               <div className="flex w-full gap-2 justify-between items-center"> 
-              <button onClick={handleVMap} className="flex w-full bg-[url('/src/assets/map-btn.png')] bg-cover p-2 items-center justify-between rounded-lg shadow-gray-500 shadow-md text-white text-md font-medium">
-                <p>Go to 3D Virtual Map</p>
+              <button onClick={handleVMap} className="flex w-full bg-[url('/src/assets/map-btn.png')] bg-cover p-3 items-center justify-between rounded-lg shadow-gray-500 shadow-md text-white text-md font-medium">
+                <p>CSU 3D Virtual Map</p>
               </button>
-              <button onClick={handleTour} className="flex w-full bg-[url('/src/assets/map-btn.png')] bg-cover p-2 items-center justify-between rounded-lg shadow-gray-500 shadow-md text-white text-md font-medium">
-                <p>Go to CSU Tour</p>
+              <button onClick={handleTour} className="flex w-full bg-[url('/src/assets/map-btn.png')] bg-cover p-3 items-center justify-between rounded-lg shadow-gray-500 shadow-md text-white text-md font-medium">
+                <p>CSU 360 Tour</p>
               </button>
               </div>
             </div>
           </div>
           <div className="flex flex-col gap-4">
-            <h1 className="px-6 text-2xl font-semibold">Recommended</h1>
+            <h1 className="px-6 text-xl font-semibold">Recommended</h1>
             <Carousel />
           </div>
 
-          <div className="flex flex-col gap-4 pb-14 mt-4">
-            <h1 className="px-6 text-2xl font-semibold">Upcoming Events</h1>
+          <div className="flex flex-col gap-4 pt-2 pb-14 mt-4">
+            <h1 className="px-6 text-xl font-semibold">Upcoming CSU - Main Events</h1>
             <EventCarousel />
           </div>
 
